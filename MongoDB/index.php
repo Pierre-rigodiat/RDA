a7ced07b81ecd4e6f4f9244e29fe8a040aede084
pns3 <pns3@P852580.nist.gov>
2012-11-06 10:43:28 -0500

    Add the correct path for the xml2json file

--- MongoDB/index.php
+++ MongoDB/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'mongodb.php';
-require_once '../xml2json/xml2json.php';
+require_once 'xml2json/xml2json.php';
 
 $db = new MongoDBStream ("127.0.0.1","test");
 $db->openDB();
