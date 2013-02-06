0d7611ea913e38dcaa1bdddb44a6bf1bf5d843f8
pnd <devnull@localhost>
2013-02-06 16:33:54 -0500

    Small text correction

--- xml_form_editor/inc/skeleton/main/public/view_xml.inc.php
+++ xml_form_editor/inc/skeleton/main/public/view_xml.inc.php
@@ -2,7 +2,7 @@
 	require_once $_SESSION['config']['_ROOT_'].'/parser/parser.inc.php';
 ?>
 <div id="featured-wrapper"><div id="featured">
-	<h1>XML View [DEMO]</h1>
+	<h1>XML View</h1>
 	<p>
 		This a preview of the XML which will be generated. Each modification you make in the form will be written in this preview.
 	</p>
