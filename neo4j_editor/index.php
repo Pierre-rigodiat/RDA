6e4a38a014abf2abaf6c67532b7e8e45be7cb136
pnd <devnull@localhost>
2013-03-08 13:27:58 -0500

    Neo4j editor now displays the edge bunding tree

--- neo4j_editor/index.php
+++ neo4j_editor/index.php
@@ -3,55 +3,32 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="resources/css/style.css">
-<title>Neo 4j tree</title>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+<title>Neo4j editor</title>
 </head>
 <body>
-<script src="http://d3js.org/d3.v3.min.js"></script>
+<script src="resources/js/lib/d3.v3.min.js"></script>
+<script src="resources/js/lib/d3.packages.js"></script>
+<h1>Neo4j editor</h1>
+<div id="tabs">
+	<ul>
+		<li><a href="#node-link">Node link</a></li>
+		<li><a href="#edge-bunding">Edge bunding</a></li>
+	</ul>
+	
+	<div id="node-link"></div>
+	<div id="edge-bunding"></div>
+</div>
+<script src="resources/js/lib/jquery-1.9.1.min.js"></script>
+<script src="resources/js/lib/jquery-ui-1.10.1.min.js"></script>
+<script src="resources/js/draw.js"></script>
 <script>
+loadDrawController();
+drawNodeTree();	
 
-var diameter = 1200;
-
-var tree = d3.layout.tree()
-    .size([360, diameter / 2 - 120])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
-
-var diagonal = d3.svg.diagonal.radial()
-    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", /*diameter*/1300)
-    .attr("height", /*diameter - 150*/1300)
-  	.append("g")
-    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-
-d3.json("resources/datas/ontology_object_type.json", function(error, root) {	
-  var nodes = tree.nodes(root),
-      links = tree.links(nodes);
-
-  var link = svg.selectAll(".link")
-      .data(links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", diagonal);
-
-  var node = svg.selectAll(".node")
-      .data(nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
-
-  node.append("circle")
-      .attr("r", 4.5);
-
-  node.append("text")
-      .attr("dy", ".31em")
-      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-      .text(function(d) { return d.name; });
+$(function() {
+	$( "#tabs" ).tabs();
 });
-
-d3.select(self.frameElement).style("height", diameter - 150 + "px");
-
 </script>
 </body>
 </html>
