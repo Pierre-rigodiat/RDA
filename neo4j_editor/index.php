51ea5966893784f875658f2150bf9da2049ad548
pnd <devnull@localhost>
2013-04-22 13:18:22 -0400

    Ontology viewer modified

--- neo4j_editor/index.php
+++ neo4j_editor/index.php
@@ -4,21 +4,24 @@
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="resources/css/style.css">
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
-<title>Neo4j editor</title>
+<title>Ontology viewer</title>
 </head>
 <body>
 <script src="resources/js/lib/d3.v3.min.js"></script>
 <script src="resources/js/lib/d3.packages.js"></script>
-<h1>Neo4j editor</h1>
-<div id="tabs">
+<h1>Ontology viewer</h1>
+<p id="disclaimer">
+	This is a partial reprensentation of the ontology living in the Neo4j database. The starting point is <b>ObjectType</b>	and it displayed up to 5 layers of related terms.
+</p>
+<div id="node-link"></div>
+<!--div id="tabs">
 	<ul>
 		<li><a href="#node-link">Node link</a></li>
 		<li><a href="#edge-bunding">Edge bunding</a></li>
 	</ul>
 	
-	<div id="node-link"></div>
-	<div id="edge-bunding"></div>
-</div>
+	
+</div-->
 <script src="resources/js/lib/jquery-1.9.1.min.js"></script>
 <script src="resources/js/lib/jquery-ui-1.10.1.min.js"></script>
 <script src="resources/js/draw.js"></script>
@@ -26,9 +29,9 @@
 loadDrawController();
 drawNodeTree();	
 
-$(function() {
+/*$(function() {
 	$( "#tabs" ).tabs();
-});
+});*/
 </script>
 </body>
 </html>
