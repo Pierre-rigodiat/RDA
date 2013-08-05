20d1af2a16d6ef3044f27deeb8d79db667997ba6
Philippe Dessauw <philippe.dessauw@nist.gov>
2013-08-05 17:08:01 -0400

    Add Periodic table

---
+++ xml_form_editor/parser/_plugins/periodicTable/PeriodicTable.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *
+ */
+/** @ignore */
+require_once $_SESSION['xsd_parser']['conf']['dirname'].'/parser/_plugins/__model__/ModuleInterface.php';
+/** @ignore */
+require_once $_SESSION['xsd_parser']['conf']['modules_dirname'].'/periodicTable/core/PeriodicTableModule.php';
+/** @ignore */
+require_once $_SESSION['xsd_parser']['conf']['modules_dirname'].'/periodicTable/view/PeriodicTableDisplay.php';
+/**
+ *
+ */
+class PeriodicTable implements ModuleInterface {
+
+    public static function initModule($tree)
+    {
+        if(!isset($_SESSION['xsd_parser']['modules']['pertable']['model']) || !isset($_SESSION['xsd_parser']['modules']['pertable']['view']))
+        {
+            // TODO Check the return codes
+            $periodicTableModule = new PeriodicTableModule($tree, true);
+            $periodicTableDisplay = new PeriodicTableDisplay($periodicTableModule, true);
+
+            $_SESSION['xsd_parser']['modules']['pertable']['model'] = serialize($periodicTableModule);
+            $_SESSION['xsd_parser']['modules']['pertable']['view'] = serialize($periodicTableDisplay);
+
+            return 0;
+        }
+        else return 1; // Module already loaded
+    }
+
+
+    public static function displayModule()
+    {
+        if(isset($_SESSION['xsd_parser']['modules']['pertable']['view']))
+        {
+            $moduleDisplay = unserialize($_SESSION['xsd_parser']['modules']['pertable']['view']);
+
+            $module = '<div class="periodic_table_module">';
+            $module .= $moduleDisplay->display();
+            $module .= '</div>';
+
+            return $module;
+        }
+        else
+        {
+            return 'Module not initialized';
+        }
+    }
+
+
+    public static function getModuleData()
+    {
+		if(isset($_SESSION['xsd_parser']['modules']['pertable']['model']))
+		{
+			$periodicTable = unserialize($_SESSION['xsd_parser']['modules']['pertable']['model']);
+			return $periodicTable -> getXmlData();
+		}
+		else
+			return null;
+    }
+
+
+    public static function __toArray()
+    {
+
+    }
+}
---
+++ xml_form_editor/parser/_plugins/periodicTable/view/PeriodicTableDisplay.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ *
+ */
+class PeriodicTableDisplay {
+
+    private $periodicTableModule;
+
+
+    // private static $PERTABLE_FILE = "/parser/_plugins/periodicTable/resources/json/periodicTable.json";
+    private static $PERTABLE_FILE = "../resources/json/periodicTable.json";
+
+	public function __construct() {
+        $argc = func_num_args();
+        $argv = func_get_args();
+
+        $this -> periodicTableModule = $argv[0];
+	}
+
+	public function update()
+	{
+		$periodicTable = unserialize($_SESSION['xsd_parser']['modules']['pertable']['model']);
+		$this -> periodicTableModule = $periodicTable;
+	}
+
+    public function display()
+    {
+        $periodicTable = null;
+        if(isset($_SESSION['xsd_parser']['modules']['pertable']['model']))
+        {
+            $periodicTable = unserialize($_SESSION['xsd_parser']['modules']['pertable']['model']);
+        }
+
+        $periodicTableCode = '<div class="module_title">Periodic table module</div>';
+
+        /**
+         * Parse JSON file
+         */
+        $rawPerTable = file_get_contents(dirname(__FILE__)."/".self::$PERTABLE_FILE);
+        $jsonPerTable = json_decode($rawPerTable, true);
+
+        $arrayPerTable = array();
+        foreach ($jsonPerTable["table"] as $perTableRow) {
+            $tmpPerTableRow = array();
+
+            if(isset($perTableRow["elements"]))
+            {
+                foreach ($perTableRow["elements"] as $chemicalElement) {
+                    $tmpPerTableRow[$chemicalElement["position"]]["name"] = $chemicalElement["name"];
+                    $tmpPerTableRow[$chemicalElement["position"]]["tag"] = $chemicalElement["tag"];
+                }
+            }
+
+            if(isset($perTableRow["lanthanoids"]))
+            {
+                foreach ($perTableRow["lanthanoids"] as $chemicalElement) {
+                    $tmpPerTableRow[$chemicalElement["position"]]["name"] = $chemicalElement["name"];
+                    $tmpPerTableRow[$chemicalElement["position"]]["tag"] = $chemicalElement["tag"];
+                }
+            }
+
+            if(isset($perTableRow["actinoids"]))
+            {
+                foreach ($perTableRow["actinoids"] as $chemicalElement) {
+                    $tmpPerTableRow[$chemicalElement["position"]]["name"] = $chemicalElement["name"];
+                    $tmpPerTableRow[$chemicalElement["position"]]["tag"] = $chemicalElement["tag"];
+                }
+            }
+
+            array_push($arrayPerTable, $tmpPerTableRow);
+            unset($tmpPerTableRow);
+        }
+
+        /**
+         * Draw periodic table
+         */
+        $periodicTableCode .= '<table id="pertable"><tbody>';
+
+        foreach ($arrayPerTable as $perTableLine) {
+            $periodicTableCode .= '<tr>';
+
+            for($i = 0; $i < 18; $i++)
+            {
+                $periodicTableCode .= '<td';
+
+                if(isset($perTableLine[$i]))
+                {
+                    $periodicTableCode .= ' class="'.$perTableLine[$i]['tag'];
+
+                    in_array($perTableLine[$i]['name'], $periodicTable -> getElementList()) ? $periodicTableCode .= " selected" : FALSE;
+
+                    $periodicTableCode .= '">';
+
+                    $periodicTableCode .= $perTableLine[$i]['name'];
+                }
+                else {
+                    $periodicTableCode .= '>';
+                }
+
+                $periodicTableCode .= '</td>';
+            }
+
+            $periodicTableCode .= '</tr>';
+        }
+
+        $periodicTableCode .= '</tbody></table>';
+
+        $periodicTableCode .= 'Selected element(s):<div id="selectedElement">';
+
+        if($periodicTable)
+        {
+            $periodicTableCode .= $this->displayList($periodicTable -> getElementList());
+        }
+
+        $periodicTableCode .= '</div>';
+
+        $periodicTableCode .= '<script type="text/javascript" src="parser/_plugins/periodicTable/controllers/js/table.js"></script>';
+        $periodicTableCode .= '<script>$(\'td[class^="x"]\').on(\'click\', toggleElement);</script>';
+
+        return $periodicTableCode;
+    }
+
+    public function displayList($elementList)
+    {
+        $elementForm = '<ul>';
+
+        foreach($elementList as $chemicalElement)
+        {
+        	$elementData = $this -> periodicTableModule -> getDataForElement($chemicalElement);
+			$qty = '';
+			$err = '';
+			$pur = '';
+
+			if($elementData !== NULL)
+			{
+				$qty = $elementData['quantity'];
+				$err = $elementData['error'];
+
+				if(isset($elementData['purity']))
+					$pur = $elementData['purity'];
+			}
+
+
+            $elementForm .= '<li>';
+            $elementForm .= '<span class="elemName">'.$chemicalElement.'</span>';
+            $elementForm .= '<ul>';
+
+            $elementForm .= '<li>Quantity <input class="qty" type="text" value="'. $qty .'"/></li>';
+            $elementForm .= '<li>Error <input class="err" type="text" value="'. $err .'"/></li>';
+            $elementForm .= '<li>Purity <input class="pur" type="text" value="'. $pur .'"/> <span class="icon remove"></span></li>';
+
+            $elementForm .= '</ul>';
+            $elementForm .= '</li>';
+        }
+
+        $elementForm .= '</ul>';
+
+        return $elementForm;
+    }
+}
