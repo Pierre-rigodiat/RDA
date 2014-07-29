59ca798195614b0efbb33ab364561883218bebd5
Sharief Youssef <sharief.youssef@nist.gov>
2014-07-29 09:09:18 -0400

    Added Django REST Swagger support for API docs auto generation

--- mdcs/mgi/settings.py
+++ mdcs/mgi/settings.py
@@ -146,9 +146,25 @@ INSTALLED_APPS = (
     'dajax',
     'dajaxice',
     'rest_framework',  # djangorestframework
+    'rest_framework_swagger', #django-rest-swagger for api documentation
     'api', # djangorestframework
-#    'multiuploader', #django-multiuploader
 )
+
+SWAGGER_SETTINGS = {
+    "exclude_namespaces": [], # List URL namespaces to ignore
+    "api_version": '0.1',  # Specify your API's version
+    "api_path": "/",  # Specify the path to your API not a root level
+    "enabled_methods": [  # Specify which methods to enable in Swagger UI
+        'get',
+        'post',
+        'put',
+        'patch',
+        'delete'
+    ],
+    "api_key": '', # An API key
+    "is_authenticated": False,  # Set to True to enforce user authentication,
+    "is_superuser": False,  # Set to True to enforce admin only access
+}
 
 #AUTH_USER_MODEL = 'mongo_auth.MongoUser'
 
