5771a5ec3f0362814470741fa20c9feec992d21a
Guillaume SOUSA <guillaume.sousa@nist.gov>
2014-12-30 13:03:45 -0500

    set DEBUG to TRUE

--- mdcs/mgi/settings.py
+++ mdcs/mgi/settings.py
@@ -34,8 +34,8 @@ PROJECT_URI = "http://www.example.com/"
 SECRET_KEY = 'ponq)(gd8hm57799)$lup4g9kyvp0l(9)k-3!em7dddn^(y)!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
