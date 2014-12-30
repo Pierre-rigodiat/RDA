f204e1c4ca34fc5f7aacae2361c24b30ebe28355
Guillaume SOUSA <guillaume.sousa@nist.gov>
2014-12-30 11:13:24 -0500

    removed contact form before new version

--- mdcs/mgi/models.py
+++ mdcs/mgi/models.py
@@ -113,12 +113,6 @@ class Instance(Document):
     password = StringField(required=True)
     status = StringField()
 
-class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField()
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
-
 class QueryResults(Document):
     results = ListField(required=True) 
     
