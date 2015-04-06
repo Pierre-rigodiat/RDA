97ce3b8d24469137594e31b186ae75b4cc1c33f1
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-04-06 11:37:54 -0400

    added version number

--- mdcs/mgi/settings.py
+++ mdcs/mgi/settings.py
@@ -20,6 +20,8 @@
 #
 ################################################################################
 
+VERSION = 1.2
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
