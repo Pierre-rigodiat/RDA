9a35f869b78a37238596d3c9eeca1f379da33b14
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-06-15 10:39:52 -0400

    Fix for Fred's bug (can run even if PYTHONHOME is set)

--- mdcs/bin/mdcs.bat
+++ mdcs/bin/mdcs.bat
@@ -2,6 +2,6 @@
 @cd /d "%~dp0"
 start cmd /k ..\mongodb\bin\mongod.exe --config ..\conf\mongodb.conf
 start cmd /k ..\jre7\bin\java.exe -jar ..\rdf\JenaServers.jar -rdfserver_endpoint "tcp://127.0.0.1:5555" -sparqlserver_endpoint "tcp://127.0.0.1:5556" -tdb_directory ..\data\ts -project_uri "http://www.example.com"
-start cmd /k ..\WinPython-32bit-2.7.6.3\python-2.7.6\python.exe ..\manage.py runserver
+start cmd /k "set PYTHONHOME=& ..\WinPython-32bit-2.7.6.3\python-2.7.6\python.exe ..\manage.py runserver"
 
 
