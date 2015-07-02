78a94c486b2972d4392b0ee21e312e9555c76ed7
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-07-02 11:41:46 -0400

    added a name to periodic table view

--- mdcs/modules/diffusion/urls.py
+++ mdcs/modules/diffusion/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^periodic-table', 'modules.diffusion.views.periodic_table_view'),
+    url(r'^periodic-table', 'modules.diffusion.views.periodic_table_view', name='Periodic Table'),
 )
