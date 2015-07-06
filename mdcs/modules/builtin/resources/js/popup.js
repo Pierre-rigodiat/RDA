ffbdfb3abef8a537f9d501d3e967ecf6fe0052a5
pnd <pnd@linux-p855672>
2015-07-06 10:46:20 -0400

    Popup module closing action corrected (destroy the popup everytime) Periodic table state saved

--- mdcs/modules/builtin/resources/js/popup.js
+++ mdcs/modules/builtin/resources/js/popup.js
@@ -1,18 +1,23 @@
 var openModule = null;
+var openPopUp = null;
 
 closePopUp = function() {
-    var $activeDialog = $('.active_dialog');
+//    var $activeDialog = $('.active_dialog');
 
-    $activeDialog.removeClass('active_dialog');
-    $activeDialog.dialog("close");
-    $activeDialog.dialog("destroy");
+    openPopUp.removeClass('active_dialog');
+    openPopUp.dialog("destroy");
 
+    openModule = null;
+    openPopUp = null;
 };
 
 var popupOptions = {
     modal: true,
     buttons: {
         Cancel: closePopUp
+    },
+    close: function(event, ui) {
+        closePopUp();
     }
 }
 
@@ -23,35 +28,19 @@ configurePopUp = function(options, getDataFunction) {
         Save: function() {
             data = getDataFunction();
             saveModuleData(openModule, data);
-            closePopUp(openModule);
+
+            closePopUp();
         }
     };
     popupOptions["buttons"] = $.extend({}, saveButton, popupOptions["buttons"]);
 }
 
 $('body').on('click', '.mod_popup .open-popup', function(event) {
-    console.log("Opening module popup...")
-
     event.preventDefault();
     openModule = $(this).parent().parent().parent();
 
-    // TODO Redo a $.ajax GET to reparse the module
-    var moduleURL = openModule.find('.moduleURL').text();
-    /*$.ajax({
-        url: '/modules/'+moduleURL,
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-        },
-        error: function() {
-            console.error('Problem @ module loading');
-        }
-    });*/
-
-    //var moduleURLRegExp = new RegExp('/', 'g');
-    //moduleURL = moduleURL.replace(moduleURLRegExp, '--');
-
     openModule.find('.mod_dialog').addClass('active_dialog');
-    $('.active_dialog').dialog(popupOptions);
+
+    openPopUp = $('.active_dialog');
+    openPopUp.dialog(popupOptions);
 });
