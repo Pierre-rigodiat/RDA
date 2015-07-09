0261f61c583c3e3ffbcaade7d0a9a086e629df7d
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-07-09 13:08:47 -0400

    url to curator's modules

--- mdcs/modules/urls.py
+++ mdcs/modules/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^diffusion/', include('modules.diffusion.urls')),
+    url(r'^curator/', include('modules.curator.urls')),
+    url(r'^diffusion/', include('modules.diffusion.urls')),    
     url(r'^test/', include('modules.test.urls')),
 )
