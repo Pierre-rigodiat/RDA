bcab35d26d09ec76c033f48f495fb80ad8e87aeb
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-09-30 13:47:41 -0400

    Removed SPARQL from MDCS windows script and added wait for mongodb server

--- mdcs/bin/mdcs.bat
+++ mdcs/bin/mdcs.bat
@@ -1,7 +1,14 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
+
 start cmd /k ..\mongodb\bin\mongod.exe --config ..\conf\mongodb.conf
-start cmd /k ..\jre7\bin\java.exe -jar ..\rdf\JenaServers.jar -rdfserver_endpoint "tcp://127.0.0.1:5555" -sparqlserver_endpoint "tcp://127.0.0.1:5556" -tdb_directory ..\data\ts -project_uri "http://www.example.com"
+
+while ! ..\mongo; do   
+	timeout /t 1
+done
+
 start cmd /k "set PYTHONHOME=& ..\WinPython-32bit-2.7.6.3\python-2.7.6\python.exe ..\manage.py runserver"
 
+timeout /t 8
 
+start http://127.0.0.1:8000
