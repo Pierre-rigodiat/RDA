db74bd1b708e9660f89c3e0eaa7a906d8d7eebf9
pnd <pnd@linux-p855672>
2015-11-25 09:39:50 -0500

    Typo correction

--- mdcs/mgi/settings.py
+++ mdcs/mgi/settings.py
@@ -72,8 +72,8 @@ else:
 
 # Replace by your own values
 from mongoengine import connect
-MONGO_MGI_USER = "mgi"
-MONGO_MGI_PASSWORD = "mgi"
+MONGO_MGI_USER = "mgi_user"
+MONGO_MGI_PASSWORD = "mgi_password"
 MONGODB_URI = "mongodb://" + MONGO_MGI_USER + ":" + MONGO_MGI_PASSWORD + "@localhost/mgi"
 connect("mgi", host=MONGODB_URI)
 
