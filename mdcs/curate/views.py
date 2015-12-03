b1b9bab9b3c40625c1edde0696759e9abd791fb3
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-12-03 13:58:42 -0500

    View for Enter Data by template's name

--- mdcs/curate/views.py
+++ mdcs/curate/views.py
@@ -31,6 +31,7 @@ from curate.forms import NewForm, OpenForm, UploadForm, SaveDataForm
 from django.http.response import HttpResponseBadRequest
 from admin_mdcs.models import permission_required
 import mgi.rights as RIGHTS
+from mgi.exceptions import MDCSError
 
 ################################################################################
 #
@@ -92,11 +93,6 @@ def curate_select_template(request):
 ################################################################################
 @permission_required(content_type=RIGHTS.curate_content_type, permission=RIGHTS.curate_edit_document, login_url='/login')
 def curate_edit_data(request):
-    template = loader.get_template('curate/curate_enter_data.html')
-    context = RequestContext(request, {
-        '': '',
-    })
-
     try:
         xml_data_id = request.GET['id']
         xml_data = XMLdata.get(xml_data_id)
@@ -131,13 +127,43 @@ def curate_edit_data(request):
             del request.session['formString']
         if 'xmlDocTree' in request.session:
             del request.session['xmlDocTree']
-
-        return HttpResponse(template.render(context))
     except:
-        # can't find the data
-        messages.add_message(request, messages.INFO, 'XML data not found.')
-        return redirect('/')
+        raise MDCSError("The document you are looking for doesn't exist.")
 
+
+################################################################################
+#
+# Function Name: curate_from_schema(request)
+# Inputs:        request -
+# Outputs:       Edit Data
+# Exceptions:    None
+# Description:   Load Data Entry Form from a template name
+#                If many template of the same name:
+#                    - take the current one if from the same version
+#                    - raise exception otherwise
+#
+#
+################################################################################
+@permission_required(content_type=RIGHTS.curate_content_type, permission=RIGHTS.curate_access, login_url='/login')
+def curate_from_schema(request):
+    try:
+        schema_name = request.GET['template']
+        templates = Template.objects(title=schema_name)
+        
+        # if the schemas are all versions of the same schema
+        if len(set(templates.values_list('templateVersion'))) == 1:
+            template_id = TemplateVersion.objects().get(pk=templates[0].templateVersion).current
+            request.session['currentTemplateID'] = template_id
+            if 'formString' in request.session:
+                del request.session['formString']
+            if 'xmlDocTree' in request.session:
+                del request.session['xmlDocTree']
+        else:
+            raise MDCSError("The selection of template by name can't be used if the MDCS contain more than one template with the same name.")
+    except:
+        raise MDCSError("The template you are looking for doesn't exist.")
+    
+    
 ################################################################################
 #
 # Function Name: curate_enter_data(request)
@@ -151,14 +177,23 @@ def curate_edit_data(request):
 @permission_required(content_type=RIGHTS.curate_content_type, permission=RIGHTS.curate_access, login_url='/login')
 def curate_enter_data(request):
     print "BEGIN curate_enter_data(request)"
-    template = loader.get_template('curate/curate_enter_data.html')
-    context = RequestContext(request, {
-        '': '',
-    })
-
-    if 'id' in request.GET:
-        return curate_edit_data(request)
-    else:
+   
+    try:
+        if 'id' in request.GET:
+            curate_edit_data(request)
+        elif 'template' in request.GET:
+            curate_from_schema(request)
+        elif 'templateid' in request.GET:
+            pass
+        
+        template = loader.get_template('curate/curate_enter_data.html')
+        context = RequestContext(request, {})
+        return HttpResponse(template.render(context))
+    except MDCSError, e:
+        template = loader.get_template('curate/errors.html')
+        context = RequestContext(request, {
+            'errors': e.message,
+        })
         return HttpResponse(template.render(context))
 
 
