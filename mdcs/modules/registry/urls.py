1fafc3dff0e5815a78ae53ff8a29a0ecb24b8535
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-12-07 09:53:25 -0500

    changed files for property class enum

--- mdcs/modules/registry/urls.py
+++ mdcs/modules/registry/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'materialType', 'modules.registry.views.registry_checkboxes_materialType', name='Registry materialType Checkboxes'),
     url(r'structuralMorphology', 'modules.registry.views.registry_checkboxes_structuralMorphology', name='Registry structuralMorphology Checkboxes'),
+    url(r'propertyClass', 'modules.registry.views.registry_checkboxes_propertyClass', name='Registry propertyClass Checkboxes'),
     url(r'relevant-date', 'modules.registry.views.relevant_date', name='Relevant Date'),
     url(r'name-pid', 'modules.registry.views.name_pid', name='Name PID'),
 )
--- mdcs/modules/registry/views.py
+++ mdcs/modules/registry/views.py
@@ -10,6 +10,12 @@ def registry_checkboxes_structuralMorphology(request):
     return RegistryCheckboxesModule(xml_tag='structuralMorphology').render(request)
 
 
+def registry_checkboxes_propertyClass(request):
+    return RegistryCheckboxesModule(xml_tag='propertyClass').render(request)
+
+
+
+
 def name_pid(request):
     return NamePIDModule().render(request)
 
