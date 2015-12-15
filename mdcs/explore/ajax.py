0848d0b9b43aa35c68dd5bd9c253f6468d32c772
Guillaume SOUSA <guillaume.sousa@nist.gov>
2015-12-15 17:39:26 -0500

    implemented refinement in ajax + updated model

--- mdcs/explore/ajax.py
+++ mdcs/explore/ajax.py
@@ -940,17 +940,20 @@ def get_results_by_instance_keyword(request):
         try:
             keyword = request.GET['keyword']
             schemas = request.GET.getlist('schemas[]')
+            refinements = refinements_to_mongo(request.GET.getlist('refinements[]'))
             onlySuggestions = json.loads(request.GET['onlySuggestions'])
         except:
             keyword = ''
             schemas = []
+            refinements = {}
             onlySuggestions = True
 
         #We get all template versions for the given schemas
         templatesVersions = Template.objects(title__in=schemas).distinct(field="templateVersion")
         #We get all templates ID, for all versions
         templatesID = TemplateVersion.objects(pk__in=templatesVersions).distinct(field="versions")
-        instanceResults = XMLdata.executeFullTextQuery(keyword, templatesID)
+        
+        instanceResults = XMLdata.executeFullTextQuery(keyword, templatesID, refinements)
         if len(instanceResults) > 0:
             if not onlySuggestions:
                 canDelete = False
@@ -2878,3 +2881,19 @@ def delete_result(request):
     
     return HttpResponse(json.dumps({}), content_type='application/javascript')
 
+
+            dot_query = "content." + dot_query
+
+
+def refinements_to_mongo(refinements):
+    try:
+        # transform the refinement in mongo query
+        mongo_queries = {}
+        for refinement in refinements:
+            splited_refinement = refinement.split(':')
+            dot_notation = splited_refinement[0]
+            value = splited_refinement[1]
+            mongo_queries[dot_notation] = value
+        return mongo_queries
+    except:
+        return []
--- mdcs/mgi/models.py
+++ mdcs/mgi/models.py
@@ -383,7 +383,7 @@ class XMLdata():
         xmldata.update({'_id': ObjectId(postID)}, {"$set":json}, upsert=False)
 
     @staticmethod
-    def executeFullTextQuery(text, templatesID, refinements=None):
+    def executeFullTextQuery(text, templatesID, refinements={}):
         #create a connection
         client = MongoClient(MONGODB_URI)
         # connect to the db 'mgi'
@@ -400,7 +400,7 @@ class XMLdata():
         else:
             full_text_query = {'schema' : {'$in': templatesID} } 
         
-        if refinements is not None:
+        if len(refinements.keys()) > 0:
             full_text_query.update(refinements)
             
         cursor = xmldata.find(full_text_query, as_class = OrderedDict)
