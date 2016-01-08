1765c3faff5c30143f76d3de5c27469eebe7aa7f
Pierre Francois <devnull@localhost>
2016-01-08 10:35:06 -0500

    Update default rights

--- mdcs/admin_mdcs/discover.py
+++ mdcs/admin_mdcs/discover.py
@@ -16,7 +16,7 @@
 #
 ################################################################################
 from django.contrib.auth.models import Permission, Group
-from mgi.rights import anonymous_group, default_group, explore_access, curate_access, api_access
+from mgi.rights import *
 
 
 def init_rules():
@@ -26,27 +26,63 @@ def init_rules():
     If the default group does not exist, creation of the group with associate permissions
     """
     try:
-        # Get or Create the Group anonymous
+        ###########################################
+        #### Get or Create the Group anonymous ####
+        ###########################################
         anonymousGroup, created = Group.objects.get_or_create(name=anonymous_group)
         if not created:
             anonymousGroup.permissions.clear()
 
-        #We add the exploration_access by default
         explore_access_perm = Permission.objects.get(codename=explore_access)
         anonymousGroup.permissions.add(explore_access_perm)
+        ###########################################
+        #### END Get or Create the Group anonymous#
+        ###########################################
 
-        # Get or Create the default basic
+        ###########################################
+        ##### Get or Create the default group #####
+        ###########################################
         defaultGroup, created = Group.objects.get_or_create(name=default_group)
         if not created:
             defaultGroup.permissions.clear()
 
-        #We add the exploration_access and curate_acces by default
+        #### EXPLORE ####
         explore_access_perm = Permission.objects.get(codename=explore_access)
-        curate_access_perm = Permission.objects.get(codename=curate_access)
-        api_access_perm = Permission.objects.get(codename=api_access)
+        explore_save_query_perm = Permission.objects.get(codename=explore_save_query)
+        explore_delete_query_perm = Permission.objects.get(codename=explore_delete_query)
         defaultGroup.permissions.add(explore_access_perm)
+        defaultGroup.permissions.add(explore_save_query_perm)
+        defaultGroup.permissions.add(explore_delete_query_perm)
+        #### END EXPLORE ####
+
+        #### CURATE ####
+        curate_access_perm = Permission.objects.get(codename=curate_access)
+        curate_view_data_save_repo_perm = Permission.objects.get(codename=curate_view_data_save_repo)
+        curate_edit_document_perm = Permission.objects.get(codename=curate_edit_document)
+        curate_delete_document_perm = Permission.objects.get(codename=curate_delete_document)
         defaultGroup.permissions.add(curate_access_perm)
+        defaultGroup.permissions.add(curate_view_data_save_repo_perm)
+        defaultGroup.permissions.add(curate_edit_document_perm)
+        defaultGroup.permissions.add(curate_delete_document_perm)
+        #### END CURATE ####
+
+        #### COMPOSE ####
+        compose_access_perm = Permission.objects.get(codename=compose_access)
+        compose_save_template_perm = Permission.objects.get(codename=compose_save_template)
+        compose_save_type_perm = Permission.objects.get(codename=compose_save_type)
+        defaultGroup.permissions.add(compose_access_perm)
+        defaultGroup.permissions.add(compose_save_template_perm)
+        defaultGroup.permissions.add(compose_save_type_perm)
+        #### END COMPOSE ####
+
+        #### API ####
+        api_access_perm = Permission.objects.get(codename=api_access)
         defaultGroup.permissions.add(api_access_perm)
+        #### END API ####
+        ###########################################
+        ##### END Get or Create the default group #
+        ###########################################
+
     except Exception, e:
         print('ERROR : Impossible to init the rules : ' + e.message)
 
