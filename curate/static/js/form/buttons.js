db9c2ed72f34e0a9e0036e4a34944e661318949e
Philippe Dessauw <pnd@nist.gov>
2016-04-18 10:44:59 -0400

    JS files cleaned

--- curate/static/js/form/buttons.js
+++ curate/static/js/form/buttons.js
@@ -1,109 +1,120 @@
-showButton = function($data, buttonClass) {
-    $data.find('.' + buttonClass + ':first').removeClass('hidden');
-};
+(function() {
+    "use strict";
+    
+    // Show the first button with the specified class
+    var showButton = function($data, buttonClass) {
+        $data.find('.' + buttonClass + ':first').removeClass('hidden');
+    };
 
-hideButton = function($data, buttonClass) {
-    $data.find('.' + buttonClass + ':first').addClass('hidden');
-};
+    // Hide the first button with the specified class
+    var hideButton = function($data, buttonClass) {
+        $data.find('.' + buttonClass + ':first').addClass('hidden');
+    };
 
-addElement = function(event) {
-    event.preventDefault();
+    // Add a new occurence of an unexisting element as a child
+    var addElement = function(event) {
+        event.preventDefault();
 
-    var $parents = $(this).parents('[id]');
+        var $parents = $(this).parents('[id]');
 
-    // The element has to have a parent
-    if ( $parents.size() == 0 ) {
-        console.error('No element to duplicate');
-        return;
-    }
+        // The element has to have a parent
+        if ( $parents.size() === 0 ) {
+            console.error('No element to duplicate');
+            return;
+        }
 
-    var $element = $($parents[0]),
-        elementId = $element.attr('id');
+        var $element = $($parents[0]),
+            elementId = $element.attr('id');
 
-    console.log("Adding " + elementId + "...")
+        console.log("Adding " + elementId + "...");
 
-    $.ajax({
-        url : "/curate/generate-bis",
-        type : "POST",
-        dataType: "html",
-        data : {
-            id: elementId
-        },
-        success: function(data){
-            var $data = $(data);
+        $.ajax({
+            url : "/curate/generate-bis",
+            type : "POST",
+            dataType: "html",
+            data : {
+                id: elementId
+            },
+            success: function(data){
+                var $data = $(data);
 
-            // Displaying the data on the screen
-            showButton($data, 'remove');
-            $element.after($data);
+                // Displaying the data on the screen
+                showButton($data, 'remove');
+                $element.after($data);
 
-            // If the element was one of occurence 0
-            if ( $element.hasClass('removed') ) {
+                if ( $element.hasClass('removed') ) {  // If the element was one of occurence 0
+                    $element.remove();
+                } else {
+                    var elementClass = $element.attr('class');
+
+                    // Update every button
+                    $.each($('.'+elementClass), function(index, occurence) {
+                        showButton($(occurence), 'remove');
+                    });
+
+                    // Updating the class with the good parent
+                    $data.removeClass();
+                    $data.addClass(elementClass);
+                }
+
+                // If new modules are included, we need to load the resources
+                initModules();
+
+                console.log("Element " + elementId + " successfully created");
+            },
+            error: function() {
+                console.error("An error occured while generating a new element.");
+            }
+        });
+    };
+
+    // Remove an occurence of a displayed element
+    var removeElement = function(event) {
+        event.preventDefault();
+
+        var $parents = $(this).parents('[id]');
+
+        // The element has to have a parent
+        if ( $parents.size() === 0 ) {
+            console.error('No element to remove');
+            return;
+        }
+
+        var $element = $($parents[0]),
+            elementId = $element.attr('id');
+
+        console.log("Removing " + elementId + "...");
+
+        $.ajax({
+            url : "/curate/remove-bis",
+            type : "POST",
+            dataType: "json",
+            data : {
+                id: elementId
+            },
+            success: function(data){
+                if ( data.code === 1 ) {  // Remove buttons need to be hidden
+                    console.log('Buttons for ' + elementId + ' need to be hidden');
+                    var elementClass = $element.attr('class');
+
+                    $.each($('.'+elementClass), function(index, occurence) {
+                        hideButton($(occurence), 'remove');
+                    });
+                } else if ( data.code === 2 ) {  // Elements need to be rewritten
+                    console.log('Element ' + elementId + ' need to be rewritten');
+                    $element.after(data.html);
+                }
+
                 $element.remove();
-            } else {
-                var elementClass = $element.attr('class');
-
-                $.each($('.'+elementClass), function(index, occurence) {
-                    showButton($(occurence), 'remove');
-                });
-
-                // Updating the class with the good parent
-                $data.removeClass();
-                $data.addClass(elementClass);
+                console.log("Element " + elementId + " successfully removed");
+            },
+            error: function() {
+                console.error("An error occured while generating a new element.");
             }
-            // If new modules are included, we need to load the resources
-            initModules();
-            console.log("Element " + elementId + " successfully created");
-        },
-        error: function() {
-            console.error("An error occured while generating a new element.");
-        }
-    });
-};
+        });
+    };
 
-removeElement = function(event) {
-    event.preventDefault();
-
-    var $parents = $(this).parents('[id]');
-
-    // The element has to have a parent
-    if ( $parents.size() == 0 ) {
-        console.error('No element to remove');
-        return;
-    }
-
-    var $element = $($parents[0]),
-        elementId = $element.attr('id');
-
-    console.log("Removing " + elementId + "...");
-
-    $.ajax({
-        url : "/curate/remove-bis",
-        type : "POST",
-        dataType: "json",
-        data : {
-            id: elementId
-        },
-        success: function(data){
-            if ( data.code === 1 ) {  // Remove buttons need to be hidden
-                console.log('Buttons for ' + elementId + ' need to be hidden');
-                var elementClass = $element.attr('class');
-
-                $.each($('.'+elementClass), function(index, occurence) {
-                    hideButton($(occurence), 'remove');
-                });
-            } else if ( data.code === 2 ) {  // Elements need to be rewritten
-                console.log('Element ' + elementId + ' need to be rewritten');
-                $element.after(data.html);
-            }
-
-            $element.remove();
-            console.log("Element " + elementId + " successfully removed");
-        },
-        error: function() {
-            console.error("An error occured while generating a new element.");
-        }
-    });
-};
-
-$(document).on('click', '.add', addElement);
-$(document).on('click', '.remove', removeElement);
+    // Buttons events
+    $(document).on('click', '.add', addElement);
+    $(document).on('click', '.remove', removeElement);
+})();
--- curate/static/js/form/choice.js
+++ curate/static/js/form/choice.js
@@ -1,121 +1,129 @@
-saveCurrentChoiceValue = function($choice) {
-    var choiceId = $choice.parent().attr('id');
-    var choiceValue = $choice.val();
-
-    $.ajax({
-        'url': '/curate/save_element',
-        'type': 'POST',
-        'dataType': 'json',
-        'data': {
-            'id': choiceId,
-            'value': choiceValue
-        },
-        'success': function(data) {
-            console.log('Element ' + choiceId + ' saved');
-        },
-        'error': function(data) {
-            console.error('An error occured when saving element ' + choiceId);
-        }
-    });
-};
-
-updateChoiceBranch = function($choice, previousChoiceValue) {
-    var choiceId = $choice.attr('id');
-    var currentChoiceValue = $choice.val();
-    var $choiceParent = $choice.parent();
-
-    console.log('Editing tree for ' + choiceId + '...');
-
-    // --------------------------------------------------
-    // Find previous element and hide it
-    var $previousValueElementList = $choiceParent.find('#'+previousChoiceValue);
-    if ( $previousValueElementList.size() != 0 ) {
-        console.log('Previous element found, hiding it...');
-
-        var $formBranch = $($previousValueElementList[0]);
-        $formBranch.addClass('hidden');
-    } else {
-        console.warn('Previous element not found, a rendering error might be the cause!');
-    }
-
-    // --------------------------------------------------
-    // Update form with the new branch
-    var $currentValueElementList = $choiceParent.find('#'+currentChoiceValue);
-    if ( $currentValueElementList.size() == 0 ) {  // Branch does not exist yet
-        console.log('Element ' + currentChoiceValue + ' does not exist, generating it...');
+(function() {
+    "use strict";
+    
+    // Save choice value to DB
+    var saveCurrentChoiceValue = function($choice) {
+        var choiceId = $choice.parent().attr('id');
+        var choiceValue = $choice.val();
 
         $.ajax({
-            'url': '/curate/generate_choice',
+            'url': '/curate/save_element',
             'type': 'POST',
-            'datatype': 'html',
+            'dataType': 'json',
             'data': {
-                'id': currentChoiceValue
+                'id': choiceId,
+                'value': choiceValue
             },
-            'success': function(data) {
-                // Add the selected branch to the form
-                var $generatedForm = $(data);
-                $choice.after($generatedForm);
-
-                // Update the id of the current option (new branch has a different id due to re-generation)
-                $choice.find("option:selected").attr('value', $generatedForm.attr('id'));
-
-                // If new modules are included, we need to load the resources
-                initModules();
-                console.log('Element ' + currentChoiceValue + ' succesfully generated');
+            'success': function() {
+                console.log('Element ' + choiceId + ' saved');
             },
-            'error': function(data) {
-                console.error('An error occured when generating element ' + currentChoiceValue);
+            'error': function() {
+                console.error('An error occured when saving element ' + choiceId);
             }
         });
-    } else {  // Branch exists (need to be toggle)
-        console.log('Element ' + currentChoiceValue+' already exists. Displaying it');
-        saveCurrentChoiceValue($choice);
+    };
 
-        var $formBranch = $($currentValueElementList[0]);
-        $formBranch.removeClass('hidden');
-    }
-};
+    // Edit the form to display the branch
+    var updateChoiceBranch = function($choice, previousChoiceValue) {
+        var choiceId = $choice.attr('id');
+        var currentChoiceValue = $choice.val();
+        var $choiceParent = $choice.parent();
 
-selectChoice = function(event) {
-    event.preventDefault();
+        console.log('Editing tree for ' + choiceId + '...');
 
-    var $choice = $(this);
-    var choiceId = $choice.parent().attr('id');
+        // --------------------------------------------------
+        // Find previous element and hide it
+        var $previousValueElementList = $choiceParent.find('#'+previousChoiceValue);
+        if ( $previousValueElementList.size() !== 0 ) {
+            console.log('Previous element found, hiding it...');
 
-    var previousChoiceValue = null;
-
-    // --------------------------------------------------
-    // Getting previous value for the choice
-    console.log('Getting previous value of element ' + choiceId + '...');
-
-    var getPreviousValue = $.ajax({
-        'url': '/curate/element_value',
-        'type': 'POST',
-        'dataType': 'json',
-        'data': {
-            'id': choiceId
+            var $previousFormBranch = $($previousValueElementList[0]);
+            $previousFormBranch.addClass('hidden');
+        } else {
+            console.warn('Previous element not found, a rendering error might be the cause!');
         }
-    });
 
-    // --------------------------------------------------
-    // Success case: Update the form
-    var getPreviousValueSuccess = function(data) {
-        previousChoiceValue = data.value;
-        console.log('Choice ' + choiceId + ' had value: ' + previousChoiceValue);
+        // --------------------------------------------------
+        // Update form with the new branch
+        var $currentValueElementList = $choiceParent.find('#'+currentChoiceValue);
+        if ( $currentValueElementList.size() === 0 ) {  // Branch does not exist yet
+            console.log('Element ' + currentChoiceValue + ' does not exist, generating it...');
 
-        updateChoiceBranch($choice, previousChoiceValue); // Update the branch
+            $.ajax({
+                'url': '/curate/generate_choice',
+                'type': 'POST',
+                'datatype': 'html',
+                'data': {
+                    'id': currentChoiceValue
+                },
+                'success': function(data) {
+                    // Add the selected branch to the form
+                    var $generatedForm = $(data);
+                    $choice.after($generatedForm);
+
+                    // Update the id of the current option (new branch has a different id due to re-generation)
+                    $choice.find("option:selected").attr('value', $generatedForm.attr('id'));
+
+                    // If new modules are included, we need to load the resources
+                    initModules();
+                    console.log('Element ' + currentChoiceValue + ' succesfully generated');
+                },
+                'error': function() {
+                    console.error('An error occured when generating element ' + currentChoiceValue);
+                }
+            });
+        } else {  // Branch exists (need to be toggle)
+            console.log('Element ' + currentChoiceValue+' already exists. Displaying it');
+            saveCurrentChoiceValue($choice);
+
+            var $currentFormBranch = $($currentValueElementList[0]);
+            $currentFormBranch.removeClass('hidden');
+        }
     };
 
-    // --------------------------------------------------
-    // Error case: Send error message
-    var getPreviousValueError = function() {
-        console.error('An error occured when reading element ' + choiceId);
+    // Action performed on a change in the selected choice
+    var selectChoice = function(event) {
+        event.preventDefault();
+
+        var $choice = $(this);
+        var choiceId = $choice.parent().attr('id');
+
+        var previousChoiceValue = null;
+
+        // --------------------------------------------------
+        // Getting previous value for the choice
+        console.log('Getting previous value of element ' + choiceId + '...');
+
+        var getPreviousValue = $.ajax({
+            'url': '/curate/element_value',
+            'type': 'POST',
+            'dataType': 'json',
+            'data': {
+                'id': choiceId
+            }
+        });
+
+        // --------------------------------------------------
+        // Success case: Update the form
+        var getPreviousValueSuccess = function(data) {
+            previousChoiceValue = data.value;
+            console.log('Choice ' + choiceId + ' had value: ' + previousChoiceValue);
+
+            updateChoiceBranch($choice, previousChoiceValue); // Update the branch
+        };
+
+        // --------------------------------------------------
+        // Error case: Send error message
+        var getPreviousValueError = function() {
+            console.error('An error occured when reading element ' + choiceId);
+        };
+
+        // --------------------------------------------------
+        // Setting up the request
+        getPreviousValue.done(getPreviousValueSuccess);
+        getPreviousValue.fail(getPreviousValueError);
     };
 
-    // --------------------------------------------------
-    // Setting up the request
-    getPreviousValue.done(getPreviousValueSuccess);
-    getPreviousValue.fail(getPreviousValueError);
-};
-
-$(document).on('change', 'select.choice', selectChoice);
+    // Choice events
+    $(document).on('change', 'select.choice', selectChoice);
+})();
