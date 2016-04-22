fde1635318db373c80782df864c2a751ecc8e896
xavier <xavier@test>
2016-04-22 15:49:10 -0400

    tests API select all schemas

--- api/tests.py
+++ api/tests.py
@@ -13,3 +13,77 @@
 # Sponsor: National Institute of Standards and Technology (NIST)
 #
 ################################################################################
+
+import requests
+from django.test import TestCase
+from datetime import datetime, timedelta
+from mgi.models import Instance
+from pymongo import MongoClient
+from mgi.settings import MONGODB_URI
+from pymongo.errors import OperationFailure
+
+class tests_token(TestCase):
+
+    def tearDown(self):
+        self.clean_db()
+
+    def clean_db(self):
+        # create a connection
+        client = MongoClient(MONGODB_URI)
+        # connect to the db 'mgi.test'
+        db = client['mgi_test']
+        # clear all collections
+        for collection in db.collection_names():
+            try:
+                if collection != 'system.indexes':
+                    db.drop_collection(collection)
+            except OperationFailure:
+                pass
+
+    def get_token(self, username, password, client_id, client_secret):
+        try:
+            url = "http://127.0.0.1:8000" + "/o/token/"
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            data = {
+                'grant_type': 'password',
+                'username': username,
+                'password': password,
+                'client_id': client_id,
+                'client_secret': client_secret
+            }
+            r = requests.post(url=url, data=data, headers=headers, timeout=int(310000))
+            if r.status_code == 200:
+                now = datetime.now()
+                delta = timedelta(seconds=int(eval(r.content)["expires_in"]))
+                expires = now + delta
+                token = Instance(name='remote_mdcs', protocol='http', address='127.0.0.1', port='8000', access_token=eval(r.content)["access_token"], refresh_token=eval(r.content)["refresh_token"], expires=expires).save()
+                return token
+            else:
+                return ''
+        except Exception, e:
+            return ''
+
+    def get_token_admin(self):
+        return self.get_token('admin', 'admin', 'client_id', 'client_secret')
+
+    def get_token_user(self):
+        return self.get_token('user', 'user', 'client_id_user', 'client_secret_user')
+
+    def test_select_all_schema_admin(self):
+        self.select_all_schema(self.get_token_admin(), False)
+
+    def test_select_all_schema_user(self):
+        self.select_all_schema(self.get_token_user(), True)
+
+    def select_all_schema(self, token, result):
+        if token == '':
+            self.assertTrue(False)
+        url = "http://127.0.0.1:8000" + "/rest/templates/select/all"
+        headers = {'Authorization': 'Bearer ' + token.access_token}
+        r = requests.get(url, data='', headers=headers)
+        if r.status_code == 200:
+            self.assertTrue(r.text != '')
+        elif r.status_code == 401:
+            self.assertTrue(result)
+        else:
+            self.assertFalse(False)
