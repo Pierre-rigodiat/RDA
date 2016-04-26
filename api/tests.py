caa9fbb6c39bd7a5b50c24689d40f6ffa4439002
xavier <xavier@test>
2016-04-26 11:13:08 -0400

    tests API delete schema

--- api/tests.py
+++ api/tests.py
@@ -14,7 +14,7 @@
 #
 ################################################################################
 
-from testing.models import TokenTest, OPERATION_GET, OPERATION_DELETE
+from testing.models import TokenTest, OPERATION_GET, OPERATION_DELETE, TemplateVersion
 
 class tests_token(TokenTest):
     def test_select_all_schema_admin(self):
@@ -77,3 +77,120 @@ class tests_token(TokenTest):
         id = str(self.createXMLData())
         r = self.doRequest(self.get_token_admin(), "/rest/explore/delete", '', {'id': id}, OPERATION_DELETE)
         self.isStatusNoContent(r)
+
+    def test_delete_schema_error_version_id_admin(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'templateVersion': 'ver', 'id':'test'}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_error_version_next_admin(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'templateVersion': 'ver', 'next':'test'}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_error_version_id_next_admin(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'templateVersion': 'ver', 'id':'test', 'next':'test'}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_error_version_not_exist_admin(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'templateVersion': 'ver'}, OPERATION_DELETE)
+        self.isStatusNotFound(r)
+
+    def test_delete_schema_error_user(self):
+        r = self.doRequest(self.get_token_user(), "/rest/templates/delete", '', {'templateVersion': 'ver', 'id':'test', 'next':'test'}, OPERATION_DELETE)
+        self.isStatusUnauthorized(r)
+
+    def test_delete_schema_error_version_already_deleted_admin(self):
+        templateVersion = self.createTemplateVersionDeleted()
+        r = self.doRequest(self.get_token_user(), "/rest/templates/delete", '', {'templateVersion': str(templateVersion.id)}, OPERATION_DELETE)
+        self.isStatusUnauthorized(r)
+
+    def test_delete_schema_version_admin(self):
+        templateVersion = self.createTemplateVersion()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'templateVersion': str(templateVersion.id)}, OPERATION_DELETE)
+        self.isStatusOK(r)
+
+    def test_delete_schema_no_id_admin(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', '', OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_bad_id_delete_schema(self):
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':'abcdefghijklmn'}, OPERATION_DELETE)
+        self.isStatusNotFound(r)
+
+    def test_delete_schema_next_not_found_admin(self):
+        template1 = self.createTemplate()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':'abcdefghijklmn'}, OPERATION_DELETE)
+        self.isStatusNotFound(r)
+
+    def test_delete_schema_2_templates_different_version_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        templateVersion2 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        template2 = self.createTemplateWithTemplateVersion(str(templateVersion2.id))
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':str(template2.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_templateversion_deleted_admin(self):
+        templateVersion1 = self.createTemplateVersionDeleted()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_templateversion_current_no_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        templateVersion1.current = str(template1.id)
+        templateVersion1.save()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_next_same_as_current_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        templateVersion1.current = str(template1.id)
+        templateVersion1.save()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':str(template1.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_template_not_current_and_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        template2 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':str(template2.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_template_and_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        template2 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        templateVersion1.current = str(template1.id)
+        templateVersion1.save()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':str(template2.id)}, OPERATION_DELETE)
+        self.isStatusNoContent(r)
+        templateVersion = TemplateVersion.objects.get(pk=template1.templateVersion)
+        self.assertTrue(templateVersion.current == str(template2.id))
+
+    def test_delete_schema_template_and_deleted_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        template2 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        templateVersion1.current = str(template1.id)
+        templateVersion1.deletedVersions.append(str(template2.id))
+        templateVersion1.save()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id), 'next':str(template2.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
+
+    def test_delete_schema_template_not_current_no_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id)}, OPERATION_DELETE)
+        self.isStatusNoContent(r)
+        templateVersion = TemplateVersion.objects.get(pk=template1.templateVersion)
+        self.assertTrue(str(template1.id) in templateVersion.deletedVersions)
+
+    def test_delete_schema_deleted_template_not_current_no_next_admin(self):
+        templateVersion1 = self.createTemplateVersion()
+        template1 = self.createTemplateWithTemplateVersion(str(templateVersion1.id))
+        templateVersion1.deletedVersions.append(str(template1.id))
+        templateVersion1.save()
+        r = self.doRequest(self.get_token_admin(), "/rest/templates/delete", '', {'id':str(template1.id)}, OPERATION_DELETE)
+        self.isStatusBadRequest(r)
