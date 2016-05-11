93ccb6716866bf4c528d91ad98c9c6aa69e00b66
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2016-05-11 11:09:46 -0400

    Add OAIAPISerializeLabelledException to manage serialization exceptions

--- oai_pmh/api/exceptions.py
+++ oai_pmh/api/exceptions.py
@@ -28,3 +28,12 @@ class OAIAPIException(Exception):
 class OAIAPILabelledException(OAIAPIException):
     def response(self):
         return Response(APIMessage.getMessageLabelled(self.message), status=self.status)
+
+class OAIAPISerializeLabelledException(OAIAPIException):
+    def __init__(self, status, errors=[],
+                 message="Error while attempting to retrieve params values. Please check your entries."):
+        super(OAIAPISerializeLabelledException, self).__init__(message, status)
+        self.errors = errors
+
+    def response(self):
+        return Response(APIMessage.getMessageSerializeLabelled(self.message, self.errors), status=self.status)
