87a31224ab4d571421a929227f9c1b6dab29b753
Guillaume SOUSA <guillaume.sousa@nist.gov>
2016-06-29 17:21:26 -0400

    Add choice.js to perform search to switch branches

--- explore/ajax.py
+++ explore/ajax.py
@@ -248,12 +248,13 @@ def generate_xsd_tree_for_querying_data(request):
         setCurrentTemplate(request, templateID)        
 
     if formString == "":
-
         try:
-            try: # custom template already exists
+            # custom template already exists
+            try:
                 custom_template = CustomTemplate.objects.get(user=str(request.user.id), template=templateID)
                 root_element_id = custom_template.root.id
-            except: # custom template doesn't exist
+            # custom template doesn't exist
+            except:
                 root_element_id = generate_form(request, xmlDocTreeStr, config=load_config())
                 custom_template = CustomTemplate(user=str(request.user.id), template=templateID, root=root_element_id).save()
 
