9f706cdeb6029ba946a3ccf49eb4f1550cd352ec
Xavier SCHMITT <xavier.schmitt@nist.gov>
2016-08-12 16:26:09 -0400

    fix script quality

--- scripts/tests/launch_quality.sh
+++ scripts/tests/launch_quality.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pylint -f parseable admin_mdcs/ mgi/ api/ oai_pmh/ user_dashboard/ compose/ explore/ utils/XSDhash/ curate/ modules/ exporter/ utils/ testing/ admin_mdcs/ | tee pylint.out
+pylint -f parseable admin_mdcs/ mgi/ api/ oai_pmh/ user_dashboard/ compose/ explore/ utils/ curate/ modules/ exporter/ utils/ testing/ | tee pylint.out
